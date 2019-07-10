bf01398d88b1e93486dbfb873d5e8fb5b4118679
Japneet Singh <jpntsingh8@gmail.com>
2019-07-10 11:09:27 -0400

    added register and styles

--- src/scripts/load_cat.js
+++ src/scripts/load_cat.js
@@ -1,4 +1,4 @@
-
+let motion = require('animejs')
 let quotes = []
 let cat
 let quote
@@ -16,4 +16,13 @@ const run = () => {
     
 }
 
+// let slideRight = () => {
+//     motion({
+//         targets: '.part-1 .part-2',
+//         translateX: -250,
+//         duration: 2000 
+//     })
+// }
+
+document.querySelector('.fa-chevron-circle-right').addEventListener('click',slideRight)
 run()
